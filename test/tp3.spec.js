e1f4046c6a07a41e5a8e2b8f3c1bad585269c168
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-07-05 11:52:38 +0200

    tp3

--- test/tp3.spec.js
+++ test/tp3.spec.js
@@ -4,17 +4,25 @@ const assert = require('assert');
 const axios = require('axios');
 const server = require('../app/tp3/tp3');
 
+let port;
+
 describe.only('tp3', function () {
-    after(function () {
+    before(async () => {
+        server.listen(0, () => { 
+            
+            port = server.address().port;
+            console.log('started on port ', port);
+         });
+    });
+    after(() => {
         server.close();
     });
-    // describe('tp3...', function () {
     it('should not find the file', async () => {
 
         // call axios with 'http://localhost:9000/asdf
         // expect file not found
         try {
-            const response = await axios.get('http://localhost:9000/asdf');
+            const response = await axios.get(`http://localhost:${port}/asdf`);
         } catch (e) {
             assert.equal(e.response.data, 'file does not exist')
             assert.equal(e.response.status, 404)
@@ -26,14 +34,13 @@ describe.only('tp3', function () {
         // call axios with 'http://localhost:9000/toto.txt
         // expect file not found
         try {
-            const response = await axios.get('http://localhost:9000/toto.txt');
+            const response = await axios.get(`http://localhost:${port}/toto.txt`);
             assert.equal(response.data, 'Toto !!!');
             assert.equal(response.status, 200);
         } catch (e) {
             assert.fail('should not throw an exception');
         }
     });
-    // });
 });
 
 
