32590a3a81732df9c543d0f40bfa905bed47ca9f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-07-05 12:20:53 +0200

    ok

--- app/tp7/src/buildcsv.ts
+++ app/tp7/src/buildcsv.ts
@@ -1,6 +1,15 @@
+import * as fs from 'fs';
+
 const i : number = 32;
-
-
 
 // kiki
 console.log('Hello from typescript ' + i);
+
+const filename = 'toto.csv';
+const stream = fs.createWriteStream(filename);
+for (let i  = 0; i < 1000000; i++) {
+    const x = Math.random();
+    const y = Math.random();
+    const chunk = [(i + '').padStart(8, "0"), x, y].join(';') + '\n';
+    stream.write(chunk);
+}
